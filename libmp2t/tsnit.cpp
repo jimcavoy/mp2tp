6b154af7fcd69c40c27171ae2279baac3c8e4925
James McAvoy <jimcavoy@thetastream.com>
2022-01-27 08:03:29 -0500

    Improved NetworkInformationTable class encapsulation by changing its layout using Pimpl idiom.

--- libmp2t/tsnit.cpp
+++ libmp2t/tsnit.cpp
@@ -17,188 +17,221 @@ const BYTE NIT_VERSION_NO = 0x3E;
 namespace lcss
 {
 
-NetworkInformationTable::NetworkInformationTable()
-{
-
-}
-
-NetworkInformationTable::~NetworkInformationTable()
-{
-
-}
-
-bool lcss::NetworkInformationTable::parse(const BYTE* table)
-{
-	if (table == nullptr)
-		return false;
-
-	nit_.push_back(table[0]);
-	UINT16 offset = nit_[0] == 0? 1 : nit_[0]; // pointer_field
-	UINT16 cur = offset;
-	UINT16 value = 0;
-
-	// stuffing bytes
-	for( auto i = 1; i < nit_[0]; i++)
-		nit_.push_back(table[i]);
-
-	if(table[cur] != 0x40) // table_id must be 0x40
-		return false;
-
-	nit_.push_back(table[cur++]); // table_id
-
-	memcpy(&value,table+cur,2);
-	UINT16 section_length = ntohs(value) & NIT_SECTION_LEN;
-	nit_.push_back(table[cur++]); //section_syntax_indicator, reserved_future_use, reserved, section_length
-	nit_.push_back(table[cur++]);
-	UINT16 crcPos = cur + section_length - 4;
-	UINT32 crc{};
-	memcpy(&crc,table+crcPos,4);
-	CRC_32_ = ntohl(crc);
-
-	nit_.push_back(table[cur++]); //network_id
-	nit_.push_back(table[cur++]);
-
-	nit_.push_back(table[cur++]); //reserved, version_number, current_next_indicator
-
-	nit_.push_back(table[cur++]); //section_number
-
-	nit_.push_back(table[cur++]); //last_section_number
-
-	memcpy(&value,table+cur,2);
-	UINT16 network_descriptors_length = ntohs(value) & NIT_SECTION_LEN;
-	nit_.push_back(table[cur++]); // reserved, network_descriptors_length
-	nit_.push_back(table[cur++]);
-
-	UINT16 end = network_descriptors_length + cur;
-
-	while(cur < end)
+	class NetworkInformationTable::Impl
 	{
-		BYTE bValue{};
-		memcpy(&bValue,table+cur,1);
-		Descriptor desc(bValue);
-		cur++;
+	public:
+		Impl() {}
+		~Impl() {}
 
-		memcpy(&bValue,table+cur,1);
-		UINT16 len = bValue;
-		cur++;
+	public:
+		std::vector<BYTE> nit_;
+		UINT32 CRC_32_{};
+		NetworkInformationTable::DescriptorArray network_descriptors_;
+		NetworkInformationTable::StreamArray streams_;
+	};
 
-		if(len > 0)
-		{
-			desc.setValue(table+cur,len);
-			cur += len;
-		}
-
-		lcss::eqDescriptor pred(desc.tag());
-		lcss::NetworkInformationTable::DescriptorArray::iterator result = find_if(network_descriptors_.begin(), network_descriptors_.end(),pred);
-		if(result == network_descriptors_.end()) // prevent adding duplicates
-			network_descriptors_.push_back(desc);
+	NetworkInformationTable::NetworkInformationTable()
+	{
+		_pimpl = std::make_unique<lcss::NetworkInformationTable::Impl>();
 	}
 
-	memcpy(&value,table+cur,2);
-	UINT16 transport_stream_loop_length = ntohs(value) & NIT_SECTION_LEN;
-	nit_.push_back(table[cur++]); // reserved_future_use, transport_stream_loop_length
-	nit_.push_back(table[cur++]);
-
-	end = transport_stream_loop_length + cur;
-
-	while(cur < end)
+	NetworkInformationTable::~NetworkInformationTable()
 	{
-		NetworkInformationTable::Stream stream;
-		memcpy(&value,table+cur,2);
-		stream.transport_stream_id_ = ntohs(value);
-		cur += 2;
-		
-		memcpy(&value,table+cur,2);
-		stream.original_network_id_ = ntohs(value);
-		cur += 2;
 
-		memcpy(&value,table+cur,2);
-		UINT16 transport_descriptors_length = ntohs(value) & NIT_SECTION_LEN;
-		cur += 2;
+	}
 
-		UINT16 tsEnd = transport_descriptors_length + cur;
+	bool NetworkInformationTable::parse(const BYTE* table)
+	{
+		if (table == nullptr)
+			return false;
 
-		while(cur < tsEnd)
+		_pimpl->nit_.push_back(table[0]);
+		UINT16 offset = _pimpl->nit_[0] == 0 ? 1 : _pimpl->nit_[0]; // pointer_field
+		UINT16 cur = offset;
+		UINT16 value = 0;
+
+		// stuffing bytes
+		for (auto i = 1; i < _pimpl->nit_[0]; i++)
+			_pimpl->nit_.push_back(table[i]);
+
+		if (table[cur] != 0x40) // table_id must be 0x40
+			return false;
+
+		_pimpl->nit_.push_back(table[cur++]); // table_id
+
+		memcpy(&value, table + cur, 2);
+		UINT16 section_length = ntohs(value) & NIT_SECTION_LEN;
+		_pimpl->nit_.push_back(table[cur++]); //section_syntax_indicator, reserved_future_use, reserved, section_length
+		_pimpl->nit_.push_back(table[cur++]);
+		UINT16 crcPos = cur + section_length - 4;
+		UINT32 crc{};
+		memcpy(&crc, table + crcPos, 4);
+		_pimpl->CRC_32_ = ntohl(crc);
+
+		_pimpl->nit_.push_back(table[cur++]); //network_id
+		_pimpl->nit_.push_back(table[cur++]);
+
+		_pimpl->nit_.push_back(table[cur++]); //reserved, version_number, current_next_indicator
+
+		_pimpl->nit_.push_back(table[cur++]); //section_number
+
+		_pimpl->nit_.push_back(table[cur++]); //last_section_number
+
+		memcpy(&value, table + cur, 2);
+		UINT16 network_descriptors_length = ntohs(value) & NIT_SECTION_LEN;
+		_pimpl->nit_.push_back(table[cur++]); // reserved, network_descriptors_length
+		_pimpl->nit_.push_back(table[cur++]);
+
+		UINT16 end = network_descriptors_length + cur;
+
+		while (cur < end)
 		{
-			BYTE bValue;
-			memcpy(&bValue,table+cur,1);
+			BYTE bValue{};
+			memcpy(&bValue, table + cur, 1);
 			Descriptor desc(bValue);
 			cur++;
 
-			memcpy(&bValue,table+cur,1);
+			memcpy(&bValue, table + cur, 1);
 			UINT16 len = bValue;
 			cur++;
 
-			if(len > 0)
+			if (len > 0)
 			{
-				desc.setValue(table+cur,len);
+				desc.setValue(table + cur, len);
 				cur += len;
 			}
 
-			stream.descriptors_.push_back(desc);
+			lcss::eqDescriptor pred(desc.tag());
+			lcss::NetworkInformationTable::DescriptorArray::iterator result = find_if(_pimpl->network_descriptors_.begin(), _pimpl->network_descriptors_.end(), pred);
+			if (result == _pimpl->network_descriptors_.end()) // prevent adding duplicates
+				_pimpl->network_descriptors_.push_back(desc);
 		}
-		streams_.push_back(stream);
+
+		memcpy(&value, table + cur, 2);
+		UINT16 transport_stream_loop_length = ntohs(value) & NIT_SECTION_LEN;
+		_pimpl->nit_.push_back(table[cur++]); // reserved_future_use, transport_stream_loop_length
+		_pimpl->nit_.push_back(table[cur++]);
+
+		end = transport_stream_loop_length + cur;
+
+		while (cur < end)
+		{
+			NetworkInformationTable::Stream stream;
+			memcpy(&value, table + cur, 2);
+			stream.transport_stream_id_ = ntohs(value);
+			cur += 2;
+
+			memcpy(&value, table + cur, 2);
+			stream.original_network_id_ = ntohs(value);
+			cur += 2;
+
+			memcpy(&value, table + cur, 2);
+			UINT16 transport_descriptors_length = ntohs(value) & NIT_SECTION_LEN;
+			cur += 2;
+
+			UINT16 tsEnd = transport_descriptors_length + cur;
+
+			while (cur < tsEnd)
+			{
+				BYTE bValue;
+				memcpy(&bValue, table + cur, 1);
+				Descriptor desc(bValue);
+				cur++;
+
+				memcpy(&bValue, table + cur, 1);
+				UINT16 len = bValue;
+				cur++;
+
+				if (len > 0)
+				{
+					desc.setValue(table + cur, len);
+					cur += len;
+				}
+
+				stream.descriptors_.push_back(desc);
+			}
+			_pimpl->streams_.push_back(stream);
+		}
+
+		return true;
 	}
 
-	return true;
-}
+	BYTE NetworkInformationTable::pointer_field() const
+	{
+		return _pimpl->nit_[0];
+	}
 
-BYTE lcss::NetworkInformationTable::pointer_field() const
-{
-	return nit_[0];
-}
+	BYTE NetworkInformationTable::table_id() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 1 : _pimpl->nit_[0];
+		if (cur > _pimpl->nit_.size() - 1)
+			return 0xFF;
+		return _pimpl->nit_[cur];
+	}
 
-BYTE lcss::NetworkInformationTable::table_id() const
-{
-	UINT16 cur = nit_[0] == 0 ? 1 : nit_[0];
-	if(cur > nit_.size()-1)
-		return 0xFF;
-	return nit_[cur];
-}
+	UINT16 NetworkInformationTable::network_id() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 0 : _pimpl->nit_[0];
+		cur += 4;
+		BYTE chVal[2]{};
+		UINT16 value;
+		chVal[0] = _pimpl->nit_[cur++];
+		chVal[1] = _pimpl->nit_[cur];
+		memcpy(&value, chVal, 2);;
+		return ntohs(value);
+	}
 
-UINT16 lcss::NetworkInformationTable::network_id() const
-{
-	UINT16 cur = nit_[0] == 0 ? 0 : nit_[0];
-	cur += 4;
-	BYTE chVal[2]{};
-	UINT16 value;
-	chVal[0] = nit_[cur++];
-	chVal[1] = nit_[cur];
-	memcpy(&value,chVal,2);;
-	return ntohs(value);
-}
+	BYTE NetworkInformationTable::version_number() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 0 : _pimpl->nit_[0];
+		cur += 6;
+		return (_pimpl->nit_[cur] & NIT_VERSION_NO) >> 1;
+	}
 
-BYTE lcss::NetworkInformationTable::version_number() const
-{
-	UINT16 cur = nit_[0] == 0 ? 0 : nit_[0];
-	cur += 6;
-	return (nit_[cur]&NIT_VERSION_NO)>>1;
-}
+	bool NetworkInformationTable::current_next_indicator() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 0 : _pimpl->nit_[0];
+		cur += 6;
+		return (_pimpl->nit_[cur] & 0x01) == 0x01 ? true : false;
+	}
 
-bool lcss::NetworkInformationTable::current_next_indicator() const
-{
-	UINT16 cur = nit_[0] == 0 ? 0 : nit_[0];
-	cur += 6;
-	return (nit_[cur]&0x01) == 0x01 ? true : false;
-}
+	BYTE NetworkInformationTable::section_number() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 0 : _pimpl->nit_[0];
+		cur += 7;
+		return _pimpl->nit_[cur];
+	}
 
-BYTE lcss::NetworkInformationTable::section_number() const
-{
-	UINT16 cur = nit_[0] == 0 ? 0 : nit_[0];
-	cur += 7;
-	return nit_[cur];
-}
+	BYTE NetworkInformationTable::last_section_number() const
+	{
+		UINT16 cur = _pimpl->nit_[0] == 0 ? 0 : _pimpl->nit_[0];
+		cur += 8;
+		return _pimpl->nit_[cur];
+	}
 
-BYTE lcss::NetworkInformationTable::last_section_number() const
-{
-	UINT16 cur = nit_[0] == 0 ? 0 : nit_[0];
-	cur += 8;
-	return nit_[cur];
-}
+	UINT32 NetworkInformationTable::CRC_32() const
+	{
+		return _pimpl->CRC_32_;
+	}
 
-UINT32 lcss::NetworkInformationTable::CRC_32() const
-{
-	return CRC_32_;
-}
+	template<typename BackInsertIter>
+	void NetworkInformationTable::network_descriptors(BackInsertIter backit) const
+	{
+		std::copy(_pimpl->network_descriptors_.begin(),
+			_pimpl->network_descriptors_.end(),
+			backit);
+	}
 
-}
+	template<typename BackInsertIter>
+	void NetworkInformationTable::transport_streams(BackInsertIter backit) const
+	{
+		std::copy(_pimpl->streams_.begin(),
+			_pimpl->streams_.end(),
+			backit);
+	}
+
+	template void NetworkInformationTable::network_descriptors<std::back_insert_iterator<std::vector<Descriptor>>>(std::back_insert_iterator<std::vector<Descriptor>>) const;
+
+	template void NetworkInformationTable::transport_streams<std::back_insert_iterator<std::vector<NetworkInformationTable::Stream>>>(std::back_insert_iterator<NetworkInformationTable::StreamArray>) const;
+
+} // lcss
--- libmp2t/tsnit.h
+++ libmp2t/tsnit.h
@@ -3,68 +3,57 @@
 #include "tspmt.h"
 
 #include <vector>
+#include <memory>
 
 namespace lcss
 {
 
-/////////////////////////////////////////////////////////////////////////////
-// Network Information Table
-// Ref: ETSI EN 300 468 V1.11.1 (2010-04), page 19
-class NetworkInformationTable
-{
-private:
-	typedef std::vector<Descriptor> DescriptorArray;
-public:
-	class Stream
+	/////////////////////////////////////////////////////////////////////////////
+	// Network Information Table
+	// Ref: ETSI EN 300 468 V1.11.1 (2010-04), page 19
+	class NetworkInformationTable
 	{
+	private:
+		typedef std::vector<Descriptor> DescriptorArray;
 	public:
-		Stream()
-			:transport_stream_id_(0)
-			,original_network_id_(0)
-		{}
-		~Stream(){}
+		class Stream
+		{
+		public:
+			Stream() {}
+			~Stream() {}
 
-		UINT16 transport_stream_id_;
-		UINT16 original_network_id_;
-		NetworkInformationTable::DescriptorArray descriptors_;
+			UINT16 transport_stream_id_{ 0 };
+			UINT16 original_network_id_{ 0 };
+			NetworkInformationTable::DescriptorArray descriptors_;
+		};
+	private:
+		typedef std::vector<NetworkInformationTable::Stream> StreamArray;
+	public:
+		NetworkInformationTable();
+		~NetworkInformationTable();
+
+		// Fields
+		BYTE	pointer_field()					const;
+		BYTE	table_id()						const;
+		UINT16	network_id()					const;
+		BYTE	version_number()				const;
+		bool	current_next_indicator()		const;
+		BYTE	section_number()				const;
+		BYTE	last_section_number()			const;
+		UINT32	CRC_32()						const;
+
+		// Methods
+		bool parse(const BYTE* table);
+
+		template<typename BackInsertIter>
+		void network_descriptors(BackInsertIter backit) const;
+
+		template<typename BackInsertIter>
+		void transport_streams(BackInsertIter backit) const;
+
+	private:
+		class Impl;
+		std::unique_ptr<Impl> _pimpl;
 	};
-private:
-	typedef std::vector<NetworkInformationTable::Stream> StreamArray;
-public:
-	NetworkInformationTable();
-	~NetworkInformationTable();
-
-	bool parse(const BYTE* table);
-
-	BYTE	pointer_field()					const;
-	BYTE	table_id()						const;
-	UINT16	network_id()					const;
-	BYTE	version_number()				const;
-	bool	current_next_indicator()		const;
-	BYTE	section_number()				const;
-	BYTE	last_section_number()			const;
-	UINT32	CRC_32()						const;
-
-	template<class BackInsertIter>
-	void network_descriptors(BackInsertIter backit) const
-	{
-		std::copy(network_descriptors_.begin(),
-			network_descriptors_.end(),
-			backit);
-	}
-
-	template<class BackInsertIter>
-	void transport_streams(BackInsertIter backit) const
-	{
-		std::copy(streams_.begin(),
-			streams_.end(),
-			backit);
-	}
-private:
-	std::vector<BYTE> nit_;
-	UINT32 CRC_32_{};
-	DescriptorArray network_descriptors_;
-	StreamArray streams_;
-};
 
 }
