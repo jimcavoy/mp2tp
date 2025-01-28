85e19bea0408a7e1c6b7e6aac00d4bc0768a6529
James McAvoy <jimcavoy@thetastream.com>
2025-01-28 09:57:33 -0500

    15 handle non mpeg 2 ts files (#16)
    * Change parse function
    
    Change parse function to return a bool.   Returns true if the function successfully parse the input stream, otherwise, false.
    
    * Add Sample MP4 File
    
    Add sample MP4 file to test parse function to return false.
    
    * Add Test Case
    
    Add test case for wrong format.
    
    * Add new option
    
    Add new option, -s,  to specify packet size.  To handle HDMV file format (*.MTS).
    
    * Change algorithm
    
    Changed algorithm to determine if the file is a well formed MPEG-2 TS format.

--- mp2tp/src/tsprsr.cpp
+++ mp2tp/src/tsprsr.cpp
@@ -6,30 +6,30 @@
 
 namespace lcss
 {
-	class TSParser::TSParserImpl
-	{
-	public:
-		TSParserImpl() 
-		{
-			_buffer.reserve(lcss::TransportPacket::TS_SIZE);
-		};
-		~TSParserImpl() = default;
+    class TSParser::TSParserImpl
+    {
+    public:
+        TSParserImpl()
+        {
+            _buffer.reserve(lcss::TransportPacket::TS_SIZE);
+        };
+        ~TSParserImpl() = default;
 
-	public:
-		TransportPacket _tspckt;
-		UINT16	_cursor{};
-		UINT64	_count{};
-		UINT32	_packetSize{ lcss::TransportPacket::TS_SIZE };
+    public:
+        TransportPacket _tspckt;
+        UINT16	_cursor{};
+        UINT64	_count{};
+        UINT32	_packetSize{ lcss::TransportPacket::TS_SIZE };
 
-		std::vector<uint8_t> _buffer{};
-	};
+        std::vector<uint8_t> _buffer{};
+    };
 }
 
 /////////////////////////////////////////////////////////////////////////////
 // TSParser
 
 lcss::TSParser::TSParser()
-	:_pimpl(std::make_unique<lcss::TSParser::TSParserImpl>())
+    :_pimpl(std::make_unique<lcss::TSParser::TSParserImpl>())
 {
 
 }
@@ -39,53 +39,64 @@ lcss::TSParser::~TSParser()
 
 }
 
-void lcss::TSParser::parse(const BYTE* stream, UINT32 len)
+bool lcss::TSParser::parse(const BYTE* stream, UINT32 len)
 {
-	UINT32 i = 0;
-	while (i < len)
-	{
-		// Malformed stream. Not on the 188 boundary. Find the sync byte.
-		if (*(stream + i) != 0x47)
-		{
-			// Converting AVCHD packets to TS packets
-			if (_pimpl->_packetSize != lcss::TransportPacket::TS_SIZE)
-			{
-				i += _pimpl->_packetSize - lcss::TransportPacket::TS_SIZE;
-			}
-			else if (_pimpl->_buffer.size() > 0 && _pimpl->_buffer.size() != lcss::TransportPacket::TS_SIZE)
-			{
-				while (_pimpl->_buffer.size() < lcss::TransportPacket::TS_SIZE)
-				{
-					_pimpl->_buffer.push_back(stream[i++]);
-				}
-			}
-			else
-			{
-				i++;
-			}
-		}
-		// The TS packet is contain in the stream buffer
-		else if (i + lcss::TransportPacket::TS_SIZE <= len)
-		{
-			if (_pimpl->_buffer.size() == 0)
-			{
-				_pimpl->_buffer.insert(_pimpl->_buffer.end(), &stream[i], &stream[i + lcss::TransportPacket::TS_SIZE]);
-				i += lcss::TransportPacket::TS_SIZE;
-			}
+    uint32_t h = 0;
+    uint32_t i = 0;
+    bool result = true;
 
-			_pimpl->_tspckt.parse(_pimpl->_buffer.data());
-			onPacket(_pimpl->_tspckt);
-			_pimpl->_tspckt = lcss::TransportPacket();
-			_pimpl->_count++;
-			_pimpl->_buffer.clear();
-		}
-		// The TS packet spans across two stream buffers
-		else if (i + lcss::TransportPacket::TS_SIZE > len)
-		{
-			while (i < len)
-				_pimpl->_buffer.push_back(stream[i++]);
-		}
-	}
+    while (i < len)
+    {
+        // Malformed stream. Not on the 188 boundary. Find the sync byte.
+        if (*(stream + i) != 0x47)
+        {
+            // The TS packet spans across two stream buffers
+            if (_pimpl->_buffer.size() > 0 && _pimpl->_buffer.size() != lcss::TransportPacket::TS_SIZE)
+            {
+                while (_pimpl->_buffer.size() < lcss::TransportPacket::TS_SIZE)
+                {
+                    _pimpl->_buffer.push_back(stream[i++]);
+                }
+            }
+            // Converting AVCHD packets to TS packets
+            else if (_pimpl->_packetSize != lcss::TransportPacket::TS_SIZE)
+            {
+                i += _pimpl->_packetSize - lcss::TransportPacket::TS_SIZE;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        // The TS packet is contain in the stream buffer
+        else if (i + lcss::TransportPacket::TS_SIZE <= len)
+        {
+            if ( h != 0 && i - h != _pimpl->_packetSize && h != i)
+            {
+                return false;
+            }
+            h = i;
+
+            if (_pimpl->_buffer.size() == 0)
+            {
+                _pimpl->_buffer.insert(_pimpl->_buffer.end(), &stream[i], &stream[i + lcss::TransportPacket::TS_SIZE]);
+                i += lcss::TransportPacket::TS_SIZE;
+            }
+
+            _pimpl->_tspckt.parse(_pimpl->_buffer.data());
+            onPacket(_pimpl->_tspckt);
+            _pimpl->_tspckt = lcss::TransportPacket();
+            _pimpl->_count++;
+            _pimpl->_buffer.clear();
+        }
+        // The TS packet spans across two stream buffers
+        else if (i + lcss::TransportPacket::TS_SIZE > len)
+        {
+            while (i < len)
+                _pimpl->_buffer.push_back(stream[i++]);
+        }
+    }
+    return result;
 }
 
 void lcss::TSParser::onPacket(lcss::TransportPacket& pckt)
@@ -95,10 +106,10 @@ void lcss::TSParser::onPacket(lcss::TransportPacket& pckt)
 
 UINT64 lcss::TSParser::packetCount() const
 {
-	return _pimpl->_count;
+    return _pimpl->_count;
 }
 
 void lcss::TSParser::setPacketSize(UINT32 sz)
 {
-	_pimpl->_packetSize = sz;
+    _pimpl->_packetSize = sz;
 }
