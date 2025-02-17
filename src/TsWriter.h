bce4fececd8cd6ecb496bf87cfbb29049273db0b
James McAvoy <jimcavoy@thetastream.com>
2025-02-17 10:00:55 -0500

    Keep Parsing
    In TSParser::parse, If the MPEG-2 TS stream contains errors, keep parsing and return false.  Let the client code determine to stop parsing.  Usually, if the first buffer read the parse function returns false, stop because it is not a MPEG-2 TS stream.

--- src/TsWriter.h
+++ src/TsWriter.h
@@ -5,39 +5,39 @@
 
 namespace lcss
 {
-	class TransportPacket;
-	class AdaptationField;
-	class ProgramAssociationTable;
-	class ProgramMapTable;
-	class PESPacket;
-	class NetworkInformationTable;
+    class TransportPacket;
+    class AdaptationField;
+    class ProgramAssociationTable;
+    class ProgramMapTable;
+    class PESPacket;
+    class NetworkInformationTable;
 }
 
 namespace mp2tpser
 {
-	class TsWriter
-	{
-	private:
-		TsWriter();
+    class TsWriter
+    {
+    private:
+        TsWriter();
 
-		friend std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
-		friend std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramMapTable& pat);
-		friend std::ostream& operator<<(std::ostream& ostrm, const lcss::PESPacket& pctk);
+        friend std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
+        friend std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramMapTable& pat);
+        friend std::ostream& operator<<(std::ostream& ostrm, const lcss::PESPacket& pctk);
 
-	public:
-		static void printHeader(std::ostream& ostrm, const lcss::TransportPacket& pckt);
+    public:
+        static void printHeader(std::ostream& ostrm, const lcss::TransportPacket& pckt);
 
-	private:
-		static void printAdaptationField(std::ostream& ostrm, const lcss::AdaptationField& adf);
-		static std::string printPCR(std::ostream& ostrm, const lcss::AdaptationField& adf);
-		static void printPAT(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
-		static void printPMT(std::ostream& ostrm, const lcss::ProgramMapTable& pmt);
-		static void printPES(std::ostream& ostrm, const lcss::PESPacket& pes);
-	};
+    private:
+        static void printAdaptationField(std::ostream& ostrm, const lcss::AdaptationField& adf);
+        static std::string printPCR(std::ostream& ostrm, const lcss::AdaptationField& adf);
+        static void printPAT(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
+        static void printPMT(std::ostream& ostrm, const lcss::ProgramMapTable& pmt);
+        static void printPES(std::ostream& ostrm, const lcss::PESPacket& pes);
+    };
 
-	std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
-	std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramMapTable& pat);
-	std::ostream& operator<<(std::ostream& ostrm, const lcss::PESPacket& pctk);
+    std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramAssociationTable& pat);
+    std::ostream& operator<<(std::ostream& ostrm, const lcss::ProgramMapTable& pat);
+    std::ostream& operator<<(std::ostream& ostrm, const lcss::PESPacket& pctk);
 }
 
 
