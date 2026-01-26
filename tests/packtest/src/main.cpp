11f43edfe945265e307dda64502d44da051d6858
James McAvoy <jimcavoy@thetastream.com>
2026-01-26 12:58:41 -0500

    Add Test Cases
    Add test cases to the packtest project.

--- tests/packtest/src/main.cpp
+++ tests/packtest/src/main.cpp
@@ -6,19 +6,18 @@
 
 using namespace std;
 
-#ifdef WIN32
-#define fprintf_s fprintf
-#else
-#define _MAX_PATH 260
-#endif
-
-void createFilename(const char* ifname, const char* ext, char* ofname);
-
 int main(int argc, char* argv[])
 {
     int ret = 0;
 
-    std::vector<unsigned char> vec(176, 0x2a);
+    if (argc != 2)
+    {
+        cerr << "Usage: packtest <size>" << endl;
+    }
+
+    int size = std::atoi(argv[1]);
+
+    std::vector<unsigned char> vec(size, 0x2a);
 
     lcss::AccessUnit au(vec.data(), vec.size(), 0xBD, 0, 0);
 
@@ -27,21 +26,8 @@ int main(int argc, char* argv[])
     size_t cc = 0;
     auto ts = packer.packetizePES(au, 255, &cc, 0);
 
+    cout << ts.size() << endl;
+
     return ret;
 }
 
-void createFilename(const char* ifname, const char* newext, char* ofname)
-{
-#ifdef WIN32
-    char drive[_MAX_DRIVE];
-    char dir[_MAX_DIR];
-    char fname[_MAX_FNAME];
-    char ext[_MAX_EXT];
-
-    _splitpath_s(ifname, drive, dir, fname, ext);
-
-    _makepath_s(ofname, _MAX_PATH, drive, dir, fname, newext);
-#else
-    sprintf(ofname, "%s.%s", ifname, newext);
-#endif
-}
