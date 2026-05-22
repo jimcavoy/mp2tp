c8080b3e4857df4a94af9e957b5c806713bbea57
James McAvoy <jimcavoy@thetastream.com>
2026-05-22 10:10:08 -0400

    Modify Test
    Add new test case, to test set and get methods for PTS, PID and packet payload.

---
+++ tests/modifytest/src/main.cpp
@@ -0,0 +1,15 @@
+#include <mp2tp/AccessUnit.h>
+#include <mp2tp/tspacker.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int ret = 0;    
+
+    return ret;
+}
+
